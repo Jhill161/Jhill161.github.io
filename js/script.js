18caa94db28b251977a8a68af2901885129b4a82
Jason Hill <Jason161@gmail.com>
2019-11-10 15:14:22 -0800

    testing stuff

--- js/script.js
+++ js/script.js
@@ -13,7 +13,10 @@ var sticky = header.offsetTop;
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+
   } else {
     header.classList.remove("sticky");
+
   }
 }
+
