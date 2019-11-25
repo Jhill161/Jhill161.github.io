a427b3ee940abda6481756835940a6f9fbf65ddb
Jason Hill <Jason161@gmail.com>
2019-11-24 16:43:50 -0800

    added experience

--- js/script.js
+++ js/script.js
@@ -1,22 +1,48 @@
-// STICKY HEADER
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the header
-var header = document.querySelector("navbar");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-
-  } else {
-    header.classList.remove("sticky");
-
+$(document).click(function(event) {
+    var clickover = $(event.target);
+    var $navbar = $('.navbar-collapse');
+    if ($('.navbar-toggler').attr('aria-expanded') == "true") {
+      $navbar.collapse('hide');
+    }
+  });
+  
+  $(document).ready(function($) {
+    var $window = $(window);
+  
+    function resize() {
+      if ($window.width() < 988) {
+        $('.exp-container').removeClass('flex-row').addClass('flex-column');
+        $('.exp-content').removeClass('flex-column').addClass('flex-row');
+      } else {
+        $('.exp-content').removeClass('flex-row').addClass('flex-column');
+        $('.exp-container').removeClass('flex-column').addClass('flex-row');
+      }
+    }
+    $window.resize(resize).trigger('resize');
+    resize();
+  });
+  
+  // Adding animation for schools when in viewport
+  $(document).scroll(function() {
+    $(".school").each(function() {
+        if (!isInView($(this))) {
+          $(this).addClass("no-before");
+          $(this).addClass("no-after");
+        } else {
+          $(this).removeClass("no-before");
+          $(this).removeClass("no-after");
+        }
+      }
+    )
+  })
+  
+  function isInView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+  
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+  
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
   }
-}
-
+  
